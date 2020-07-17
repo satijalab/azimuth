0bb4432a8f61203dedafe4289c3f758b30a0b6b4
Paul Hoffman <mojaveazure@users.noreply.github.com>
2020-07-16 22:05:08 -0400

    Update mapper app with new version Actually do the mapping Properly import function from dependent packages Remove unused files and functions

--- R/app.R
+++ R/app.R
@@ -1,97 +1,279 @@
-#' @include ui.R
-#' @include input.R
-#' @include servers.R
+#' @include zzz.R
+#' @importFrom htmltools tagList
+#' @importFrom shinyjs useShinyjs disabled
+#' @importFrom shiny fluidPage sidebarLayout sidebarPanel fileInput
+#' actionButton selectInput downloadButton mainPanel tabsetPanel tabPanel
+#' plotOutput verbatimTextOutput shinyApp
 #'
 NULL
 
-#' @importFrom shiny fluidPage sidebarLayout sidebarPanel mainPanel
-#'
-ui <- fluidPage(
-  title = "SeuratMapper",
-  sidebarLayout(
-    sidebarPanel =  sidebarPanel(
-      # tenxH5Input(id = "tenxh5")
-      fileInput(inputId = "tenxh5", label = "10X HDF5 Input")
-    ),
-    mainPanel = mainPanel(
-      # dimPlotUI(id = "reference"),
-      # dimPlotUI(id = "user")
-      shiny::plotOutput(outputId = "reference"),
-      shiny::plotOutput(outputId = "user")
+app.title <- 'Azimuth'
+
+ui <- tagList(
+  useShinyjs(),
+  fluidPage(
+    title = app.title,
+    sidebarLayout(
+      sidebarPanel = sidebarPanel(
+        fileInput(inputId = "file", label = app.title),
+        disabled(actionButton(inputId = "proc1", label = "Preprocess Input")),
+        disabled(actionButton(inputId = "map", label = "Map cells to reference")),
+        disabled(selectInput(
+          inputId = 'feature',
+          label = 'Feature',
+          choices = '',
+          selectize = FALSE,
+          width = '100%'
+        )),
+        disabled(downloadButton(
+          outputId = 'dlumap',
+          label = 'Download UMAP RDS'
+        )),
+        disabled(downloadButton(
+          outputId = 'dlpred',
+          label = 'Download the predicted IDs and scores'
+        ))
+      ),
+      mainPanel = mainPanel(tabsetPanel(
+        id = "tabs",
+        tabPanel(
+          title = "Preprocessing and Quality Control",
+          plotOutput(outputId = "qcvln"),
+          verbatimTextOutput(outputId = "sct"),
+          verbatimTextOutput(outputId = "mapping")
+        )
+      ))
     )
   )
 )
 
-#' @importFrom SeuratDisk LoadH5Seurat
+#' Server function for the mapping app
+#'
+#' @param input,output,session Required Shiny app server parameters
+#'
+#' @return The shiny server logic
+#'
+#' @name AzimuthServer
+#' @rdname AzimuthServer
+#'
+#' @importFrom shinyjs enable
+#' @importFrom Seurat Idents<- SCTransform VariableFeatures DefaultAssay Idents
+#' RunUMAP VlnPlot DimPlot Reductions FeaturePlot
+#' @importFrom shiny reactiveValues appendTab tabPanel plotOutput observeEvent
+#' withProgress setProgress renderText updateSelectInput updateTabsetPanel
+#' renderPlot downloadHandler
+#'
+#' @keywords internal
 #'
 server <- function(input, output, session) {
-  # Load the reference object
-  reference <- LoadH5Seurat(
-    file = system.file(
-      file.path('references', 'pbmc3k_final.h5Seurat'),
-      package = 'SeuratMapper',
-      mustWork = TRUE
-    ),
-    assays = "data",
-    reductions = "umap",
-    graphs = FALSE,
-    images = FALSE,
-    meta.data = FALSE,
-    commands = FALSE
+  app.env <- reactiveValues(object = NULL)
+  refs <- LoadReference(path = "http://saucyx220.nygenome.org")
+  Idents(object = refs$reference) <- 'id'
+  appendTab(
+    inputId = 'tabs',
+    tab = tabPanel(
+      title = "Mapped Data",
+      plotOutput(outputId = 'refdim'),
+      plotOutput(outputId = 'objdim')
+    )
   )
-  plot.env <- shiny::reactiveValues()
-  # Load the user-supplied data
-  observeEvent(
-    eventExpr = input$tenxh5,
+  # React to events
+  observeEvent( # Load the data
+    eventExpr = input$file,
     handlerExpr = {
-      # print(input$tenxh5)
-      # print(class(input$tenxh5))
-      mat <- Seurat::Read10X_h5(input$tenxh5$datapath)
-      if (is.list(x = mat)) {
-        mat <- mat[[1]]
-      }
-      object <- Seurat::CreateSeuratObject(counts = mat)
-      object <- Seurat::NormalizeData(object)
-      object <- Seurat::FindVariableFeatures(object)
-      object <- Seurat::ScaleData(object)
-      object <- Seurat::RunPCA(object)
-      object <- Seurat::RunUMAP(object, dims = 1:30)
-      plot.env$user <- object
+      withProgress(
+        message = "Reading input",
+        expr = {
+          setProgress(value = 0)
+          app.env$object <- LoadFileInput(path = input$file$datapath)
+          setProgress(value = 1)
+        }
+      )
     }
   )
-  # counts <- tenxH5Server(id = "tenxh5")
-  # object <- reactive(x = {
-  #   obj <- CreateSeuratObject(counts = counts())
-  #   message("Created Seurat object")
-  #   obj
-  # })
-  # Create plots
-  # dimPlotServer(id = "reference", object = reactive(x = reference))
-  # dimPlotServer(id = "user", object = reactive(reference))
-  output$reference <- shiny::renderPlot(
-    Seurat::DimPlot(object = reference)
+  observeEvent(eventExpr = input$file, handlerExpr = enable(id = 'proc1'))
+  observeEvent( # Process the user data
+    eventExpr = input$proc1,
+    handlerExpr = {
+      # Run SCTransform and enable mapping
+      withProgress(
+        message = "Normalizing with SCTransform",
+        expr = {
+          output$sct <- renderText(expr = NULL)
+          setProgress(value = 0)
+          app.env$object <- suppressWarnings(expr = SCTransform(
+            object = app.env$object,
+            residual.features = rownames(x = refs$reference),
+            ncells = min(3000, ncol(x = app.env$object))
+          ))
+          setProgress(value = 1)
+          output$sct <- renderText(expr = "SCTransform complete")
+        }
+      )
+      enable(id = "map")
+      # Enable the feature explorer
+      enable(id = 'feature')
+      updateSelectInput(
+        session = session,
+        inputId = 'feature',
+        label = 'Feature',
+        choices = FilterFeatures(features = rownames(x = app.env$object)),
+        selected = VariableFeatures(object = app.env$object)[1]
+      )
+      appendTab(
+        inputId = 'tabs',
+        tabPanel(
+          title = "Feature Explorer",
+          plotOutput(outputId = 'fvln'),
+          plotOutput(outputId = 'fdim')
+        )
+      )
+    }
   )
-  output$user <- shiny::renderPlot(expr = Seurat::DimPlot(plot.env$user))
+  observeEvent( # Map data
+    eventExpr = input$map,
+    handlerExpr = {
+      withProgress(
+        message = 'Mapping data',
+        expr = {
+          setProgress(value = 0, message = "Finding anchors")
+          # TODO: export FindTransferAnchors_Fast
+          anchors <- Seurat:::FindTransferAnchors_Fast(
+            reference = refs$reference,
+            query = app.env$object,
+            reference.nn = refs$index,
+            reference.nnidx = refs$index$annoy_index,
+            reference.assay = DefaultAssay(object = refs$reference),
+            npcs = NULL,
+            k.filter = NA,
+            query.assay = 'SCT',
+            reference.reduction = 'spca',
+            normalization.method = 'SCT',
+            features = rownames(x = refs$reference),
+            dims = 1:50
+          )
+          setProgress(value = 0.6, message = 'Integrating data')
+          # TODO: export IngestNewData_Fase
+          ingested <- Seurat:::IngestNewData_Fast(
+            reference = refs$reference,
+            query = app.env$object,
+            dims = 1:50,
+            transfer.anchors = anchors,
+            reference.nnidx = refs$index$annoy_index,
+            transfer.labels = Idents(object = refs$reference)
+          )
+          setProgress(value = 0.8, message = "Running UMAP transform")
+          cells <- colnames(x = app.env$object)
+          app.env$object$predicted.id <- ingested$predicted.id[cells]
+          app.env$object$predicted.id.score <- ingested$predicted.id.score[cells]
+          app.env$object[['umap.proj']] <- RunUMAP(
+            object = ingested[['query_ref.nn']],
+            reduction.model = refs$reference[['jumap']],
+            reduction.key = 'ProjU_'
+          )
+          setProgress(value = 1)
+        }
+      )
+      # Add the predicted ID and score to the plots
+      Idents(object = app.env$object) <- 'predicted.id'
+      updateSelectInput(
+        session = session,
+        inputId = 'feature',
+        label = 'Feature',
+        choices = c(
+          'predicted.id.score',
+          FilterFeatures(features = rownames(x = app.env$object))
+        ),
+        selected = 'predicted.id.score'
+      )
+      updateTabsetPanel(
+        session = session,
+        inputId = 'tabs',
+        selected = 'Mapped Data'
+      )
+      # Enable downloads
+      enable(id = 'dlumap')
+      enable(id = 'dlpred')
+    }
+  )
+  # Plots
+  output$qcvln <- renderPlot(expr = {
+    if (!is.null(x = app.env$object)) {
+      qc <- paste0(
+        c('nCount_', 'nFeature_'),
+        DefaultAssay(object = app.env$object)
+      )
+      VlnPlot(object = app.env$object, features = qc)
+    }
+  })
+  output$refdim <- renderPlot(expr = {
+    DimPlot(object = refs$reference)
+  })
+  output$objdim <- renderPlot(expr = {
+    if (!is.null(x = app.env$object)) {
+      if (length(x = Reductions(object = app.env$object))) {
+        DimPlot(object = app.env$object)
+      }
+    }
+  })
+  output$fvln <- renderPlot(expr = {
+    if (!is.null(x = app.env$object)) {
+      avail <- c(rownames(x = app.env$object), colnames(x = app.env$object[[]]))
+      if (input$feature %in% avail) {
+        VlnPlot(object = app.env$object, features = input$feature)
+      }
+    }
+  })
+  output$fdim <- renderPlot(expr = {
+    if (!is.null(x = app.env$object)) {
+      if (length(x = Reductions(object = app.env$object))) {
+        FeaturePlot(object = app.env$object, features = input$feature)
+      }
+    }
+  })
+  # Downloads
+  output$dlumap <- downloadHandler(
+    filename = paste0(tolower(x = app.title), '_umap.Rds'),
+    content = function(file) {
+      if (!is.null(x = app.env$object)) {
+        if ('umap.proj' %in% Reductions(object = app.env$object)) {
+          saveRDS(object = app.env$object[['umap.proj']], file = file)
+        }
+      }
+    }
+  )
+  output$dlpred <- downloadHandler(
+    filename = paste0(tolower(x = app.title), '_pred.tsv'),
+    content = function(file) {
+      req <- c('predicted.id', 'predicted.id.score')
+      if (all(req %in% colnames(x = app.env$object[[]]))) {
+        write.table(
+          x = data.frame(
+            cell = colnames(x = app.env$object),
+            predicted.id = app.env$object$predicted.id,
+            predicted.score = app.env$object$predicted.id.score,
+            stringsAsFactors = FALSE
+          ),
+          file = file,
+          quote = FALSE,
+          row.names = FALSE,
+          col.names = TRUE,
+          sep = '\t'
+        )
+      }
+    }
+  )
 }
 
-app <- shinyApp(ui = ui, server = server)
-
-# TODO:
-# Download:
-# umap coordinates
-# number of cells per identity
-# prediction score
-# feature plot on query (separate tab?)
-
-#' Launch the Mapper App
+#' Launch the mapping app
 #'
-#' @return None, launches the Shiny Mapper app
+#' @return None, launches the mapping Shiny app
 #'
-#' @importFrom withr with_options
+#' @importFrom shiny runApp
 #'
 #' @export
 #'
-Mapper <- function() {
-  runApp(appDir = app)
+AzimuthApp <- function() {
+  runApp(appDir = shinyApp(ui = ui, server = server))
   return(invisible(x = NULL))
 }
--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,177 @@
 #'
 "_PACKAGE"
 
+#' Create an annoy index
+#'
+#' @note Function exists because it's not exported from \pkg{uwot}
+#'
+#' @param name,ndim Paramters
+#'
+#' @return An nn index object
+#'
+#' @importFrom methods new
+#' @importFrom RcppAnnoy AnnoyAngular AnnoyManhattan AnnoyEuclidean AnnoyHamming
+#'
+#' @keywords internal
+#'
+CreateAnn <- function(name, ndim) {
+  return(switch(
+    EXPR = name,
+    cosine = new(Class = AnnoyAngular, ndim),
+    manhattan = new(Class = AnnoyManhattan, ndim),
+    euclidean = new(Class = AnnoyEuclidean, ndim),
+    hamming = new(Class = AnnoyHamming, ndim),
+    stop("BUG: unknown Annoy metric '", name, "'")
+  ))
+}
+
+#' Sanitize feature names for \code{\link[shiny]{selectInput}}
+#'
+#' \code{\link[shiny]{selectInput}} has some limitiations with biological
+#' feature names. This function sanitizes feature names according to the
+#' following rules:
+#' \itemize{
+#'  \item Names matching \dQuote{\\.1} are \strong{removed}
+#' }
+#'
+#' @param features A character vector of feature names
+#'
+#' @return \code{features}, but sanitized
+#'
+#' @keywords internal
+#'
+#' @seealso \code{\link[shiny]{selectInput}}
+#'
+FilterFeatures <- function(features) {
+  return(sort(x = grep(
+    pattern = '\\.1',
+    x = features,
+    value = TRUE,
+    invert = TRUE
+  )))
+}
+
+#' Load file input into a \code{Seurat} object
+#'
+#' @param path Path to input data
+#'
+#' @return A \code{Seurat} object
+#'
+#' @importFrom tools file_ext
+#' @importFrom SeuratDisk LoadH5Seurat
+#' @importFrom Seurat Read10X_h5 CreateSeuratObject
+#'
+#' @keywords internal
+#'
+LoadFileInput <- function(path) {
+  type <- tolower(x = file_ext(x = path))
+  return(switch(
+    EXPR = type,
+    'h5' = {
+      mat <- Read10X_h5(filename = path)
+      if (is.list(x = mat)) {
+        mat <- mat[[1]]
+      }
+      CreateSeuratObject(counts = mat)
+    },
+    'rds' = readRDS(file = path),
+    'h5seurat' = LoadH5Seurat(file = path),
+    stop("Unknown file type: ", type, call. = FALSE)
+  ))
+}
+
+#' Load the reference RDS files
+#'
+#' Read in a reference \code{\link[Seurat]{Seurat}} object and annoy index. This
+#' function can read either from URLs or a file path. In order to read properly,
+#' there must be the following files:
+#' \itemize{
+#'  \item \dQuote{ref.Rds} for the reference \code{Seurat} object
+#'  \item \dQuote{idx.Rds} for the R-based index object
+#'  \item \dQuote{idx.annoy} for the
+#'  \link{RcppAnnoy::AnnoyIndex}{RcppAnnoy-based} index object
+#' }
+#'
+#' @param path Path or URL to the two RDS files
+#' @param seconds Timeout to check for URLs in seconds
+#'
+#' @return A list with two entries:
+#' \describe{
+#'  \item{\code{reference}}{The reference \code{\link[Seurat]{Seurat}} object}
+#'  \item{\code{index}}{
+#'    A list with index information, includes an
+#'    \code{\link[RcppAnnoy]{AnnoyIndex}} object
+#'  }
+#' }
+#'
+#' @importFrom httr build_url parse_url modify_url status_code GET timeout
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#' # Load from a URL
+#' ref <- LoadReference("http://saucyx220.nygenome.org")
+#' # Load from a directory
+#' ref2 <- LoadReference("/var/www/html")
+#' }
+#'
+LoadReference <- function(path, seconds = 10L) {
+  ref.name <- 'ref.Rds'
+  idx.name <- 'idx.Rds'
+  ann.name <- 'idx.annoy'
+  uri <- build_url(url = parse_url(url = path))
+  if (grepl(pattern = '^://', x = uri)) {
+    if (!dir.exists(paths = path)) {
+      stop("Cannot find directory ", path, call. = FALSE)
+    }
+    reference <- file.path(path, ref.name)
+    index <- file.path(path, idx.name)
+    annoy <- file.path(path, ann.name)
+    if (!file.exists(reference)) {
+      stop("Cannot find reference RDS in ", path, call. = FALSE)
+    } else if (!file.exists(index)) {
+      stop("Cannot find index RDS in ", path, call. = FALSE)
+    } else if (!file.exists(annoy)) {
+      stop("Cannot find index annoy in ", path, call. = FALSE)
+    }
+  } else {
+    Online <- function(url) {
+      return(identical(
+        x = status_code(x = GET(url = url, timeout(seconds = seconds))),
+        y = 200L
+      ))
+    }
+    uri <- parse_url(url = uri)
+    ref.uri <- modify_url(url = uri, path = ref.name)
+    idx.uri <- modify_url(url = uri, path = idx.name)
+    ann.uri <- modify_url(url = uri, path = ann.name)
+    if (!Online(url = ref.uri)) {
+      stop("Cannot find reference RDS object at ", path, call. = FALSE)
+    } else if (!Online(url = idx.uri)) {
+      stop("Cannot find index RDS object at ", path, call = FALSE)
+    } else if (!Online(url = ann.uri)) {
+      stop("Cannot find index annoy object at ", path, call. = FALSE)
+    }
+    reference <- url(description = ref.uri)
+    index <- url(description = idx.uri)
+    annoy <- tempfile()
+    download.file(url = ann.uri, destfile = annoy, quiet = TRUE)
+    on.exit(expr = {
+      close(con = reference)
+      close(con = index)
+      unlink(x = annoy)
+    })
+  }
+  nn <- readRDS(file = index)
+  annoy.index <- CreateAnn(name = nn$metric, ndim = nn$ndim)
+  annoy.index$load(file = annoy)
+  nn$annoy_index <- annoy.index
+  return(list(
+    reference = readRDS(file = reference),
+    index = nn
+  ))
+}
 
 GetDefaultArguments <- function(f, method = NULL) {
   UseMethod(generic = 'GetDefaultArguments', object = f)
