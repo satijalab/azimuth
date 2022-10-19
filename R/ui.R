8b0e2240b2ee5b3041f512db933c9a61403f667a
Gesmira <gesmiramolla@gmail.com>
2022-10-19 11:11:15 -0400

    going back to og

--- R/ui.R
+++ R/ui.R
@@ -715,46 +715,44 @@ AzimuthBridgeUI <- tagList(
               tableOutput(outputId = 'table.qc'),
               width = 4
             ), 
-            verticalLayout(
-              box(
-                  title = p(
-                    'Overlap QC',
-                    bsButton(
-                      inputId = 'q2',
-                      label = '',
-                      icon = icon(name = 'question'),
-                      style = 'info',
-                      size = 'extra-small'
-                    )
+            box(
+                title = p(
+                  'Overlap QC',
+                  bsButton(
+                    inputId = 'q2',
+                    label = '',
+                    icon = icon(name = 'question'),
+                    style = 'info',
+                    size = 'extra-small'
+                  )
+                ),
+                bsPopover(
+                  id = 'q2',
+                  title = 'Overlap QC',
+                  content = paste(
+                    'Overlap QC Explanation'
                   ),
-                  bsPopover(
-                    id = 'q2',
-                    title = 'Overlap QC',
-                    content = paste(
-                      'Overlap QC Explanation'
-                    ),
-                    placement = 'right',
-                    trigger = 'focus',
-                    options = list(container = 'body')
-                  ),
-                  plotOutput(outputId = 'dist.qc'),
-                  width = 4
-              ), 
-              valueBoxOutput(outputId = 'valuebox.overlap', width = 3),
-              div(
-                id = 'overlap_popup',
-                valueBoxOutput(outputId = "valuebox_overlap", width = 3),
-                bsTooltip(id = "valuebox_overlap", title = "Click for more info", placement = "top", trigger = 'hover'),
-              ),
-            ),
+                  placement = 'right',
+                  trigger = 'focus',
+                  options = list(container = 'body')
+                ),
+                plotOutput(outputId = 'dist.qc'),
+                width = 4
+            ), 
           ),
           fluidRow(
             valueBoxOutput(outputId = 'valuebox.upload', width = 3),
+            valueBoxOutput(outputId = 'valuebox.overlap', width = 3),
             valueBoxOutput(outputId = 'valuebox.preproc', width = 3),
             div(
               id = 'panchors_popup',
               valueBoxOutput(outputId = "valuebox_panchors", width = 3),
               bsTooltip(id = "valuebox_panchors", title = "Click for more info", placement = "top", trigger = 'hover'),
+            ),
+            div(
+              id = 'overlap_popup',
+              valueBoxOutput(outputId = "valuebox_overlap", width = 3),
+              bsTooltip(id = "valuebox_overlap", title = "Click for more info", placement = "top", trigger = 'hover')
             ),
             div(
               id = 'mappingqcstat_popup',
