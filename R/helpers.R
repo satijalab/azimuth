667ab43cf517cbca848f312b4b78d05b06203cde
AustinHartman <hartmana99@gmail.com>
2023-02-02 14:38:35 -0500

    add gene conversion function

--- R/helpers.R
+++ R/helpers.R
@@ -113,6 +113,49 @@ ConvertGeneNames <- function(object, reference.names, homolog.table) {
   }
 }
 
+ConvertEnsembleToSymbol <- function(
+    mat,
+    species = c('human', 'mouse')
+) {
+  species <- match.arg(arg = species)
+  if (species == 'human') {
+    database <- 'hsapiens_gene_ensembl'
+    symbol <- 'hgnc_symbol'
+
+  } else if (species == 'mouse') {
+    database <- 'mmusculus_gene_ensembl'
+    symbol <- 'mgi_symbol'
+
+  } else {
+    stop('species name not found')
+  }
+
+  library("biomaRt")
+  library("dplyr")
+
+  name_df <- data.frame(gene_id = c(rownames(mat)))
+  name_df$orig.id <- name_df$gene_id
+  #make this a character, otherwise it will throw errors with left_join
+  name_df$gene_id <- as.character(name_df$gene_id)
+  # in case it's gencode, this mostly works
+  #if ensembl, will leave it alone
+  name_df$gene_id <- sub("[.][0-9]*","",name_df$gene_id)
+  mart <- useDataset(dataset = database, useMart("ensembl"))
+  genes <-  name_df$gene_id
+  gene_IDs <- getBM(filters= "ensembl_gene_id",
+                    attributes= c("ensembl_gene_id", symbol),
+                    values = genes,
+                    mart= mart)
+  gene.df <- left_join(name_df, gene_IDs, by = c("gene_id"="ensembl_gene_id"))
+  rownames(gene.df) <- make.unique(gene.df$orig.id)
+  gene.df <- gene.df[rownames(mat),]
+  gene.df <-gene.df[gene.df[,symbol] != '',]
+  gene.df <- gene.df[ !is.na(gene.df$orig.id),]
+  mat.filter <- mat[gene.df$orig.id,]
+  rownames(mat.filter) <- make.unique(gene.df[,symbol])
+  return(mat.filter)
+}
+
 # Return CSS styling for hover box on interactive plots
 #
 # @param x X hover position (hover$coords_css$x)
