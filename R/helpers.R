a00a34c1b254aaf5a630f90b4b78983b02d4dc04
Gesmira <gesmiramolla@gmail.com>
2023-03-20 19:35:56 -0400

    changing to v3 assay to make plot object

--- R/helpers.R
+++ R/helpers.R
@@ -616,9 +616,9 @@ LoadReference <- function(path, seconds = 10L) {
     i = 1, j = 1, x = 0, dims = c(1, nrow(x = plotref.dr)),
     dimnames = list("placeholder", Cells(x = plotref.dr))
   )
-  plot <- CreateSeuratObject(
-    counts = cm
-  )
+  op <- options(Seurat.object.assay.version = "v3")
+  on.exit(expr = options(op), add = TRUE)
+  plot <- CreateSeuratObject(counts = cm)
   plot[["refUMAP"]] <- plotref.dr
   DefaultAssay(plot[["refUMAP"]]) <- DefaultAssay(plot)
   plot <- AddMetaData(object = plot, metadata = Misc(object = plotref.dr, slot = "plot.metadata"))
